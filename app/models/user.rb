87023083f56694f1a2c61da807205d24e2a0a0c1
nikhilc <nikhilc.developer@gmail.com>
2016-03-24 15:50:27 +0000

    Added account model and its method respectively in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,28 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  validate :email_is_unique, on: :create
+  after_create :create_account
+ 
          
   def confirmation_required?
     false
   end
+  
+  #Create an account, everytime, a new user is created
+  def create_account
+    account = Account.new(:email => email)
+    account.save!
+  end
+  
+  private
+    #Email should be unique in Account model
+    def email_is_unique
+      return false unless self.errors[:email].empty?
+      unless Account.find_by_email(email).nil?
+        errors.add(:email, "is already taken")
+      end
+    end
+    
 end
